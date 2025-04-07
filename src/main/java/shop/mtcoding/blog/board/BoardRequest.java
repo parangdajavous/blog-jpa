e0662641aa14e3d91c9dba77da05bb5f50a27e6c
parang <parang@naver.com>
2025-04-07 10:07:37 +0900

    board save

---
+++ src/main/java/shop/mtcoding/blog/board/BoardRequest.java
@@ -0,0 +1,24 @@
+package shop.mtcoding.blog.board;
+
+import lombok.Data;
+import shop.mtcoding.blog.user.User;
+
+public class BoardRequest {
+
+    @Data
+    public static class SaveDTO {
+        private String title;
+        private String content;
+        private String isPublic;
+
+        public Board toEntity(User user) {
+            return Board.builder()
+                    .title(title)
+                    .content(content)
+                    .isPublic(isPublic == null ? false : true)
+                    .user(user)   // user 객체 필요
+                    .build();
+        }
+
+    }
+}
